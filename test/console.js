fce900e586943f8d595d2c64196a6b2fa21f0d4b
shsssskn <s.nishino@gemcook.com>
2018-06-11 14:24:51 +0900

    [add] ### test no-console

---
+++ test/console.js
@@ -0,0 +1,8 @@
+console.log('expect warn');
+console.info('expect ok');
+console.error('expect ok');
+
+// /Users/shsssskn/Documents/eslint-config-gemcook/test/console.js
+//   1:1  warning  Unexpected console statement  no-console
+//
+// ✖ 1 problem (0 errors, 1 warning)
